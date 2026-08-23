1abee3d1a1fa72fb41e621f4e1d6501fdf0dae7e
samyax09 <samyaxdixit8@gmail.com>
2026-08-24 00:25:50 +0530

    Add string-commands code

---
+++ string-commands/main.py
@@ -0,0 +1,13 @@
+# strings
+# strings are immutable
+a = "Samyax will become a great programmer!!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("samyax","god"))
+print(a.split(" "))
+print(a.capitalize())
+print(a.count("!"))
+print(a.title())
+print(a.swapcase())
