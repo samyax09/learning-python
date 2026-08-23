e2c3e282749199df2b922387da4c403aee2cd6a4
samyax09 <samyaxdixit8@gmail.com>
2026-08-24 00:25:51 +0530

    Add basic-user-input-password code

---
+++ basic-user-input-password/main.py
@@ -0,0 +1,5 @@
+User = int(input("Enter the Password: "))
+if User == 202612:
+  print("Correct Password!!\nAuthorization Granted!!")
+else :
+  print ("Incorrect Password!!\nAuthorization Denied!!")
