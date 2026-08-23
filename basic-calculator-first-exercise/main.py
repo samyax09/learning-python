8ba655ab6004d3a7bcffa3162431e49485ea8d3c
samyax09 <samyaxdixit8@gmail.com>
2026-08-24 00:25:48 +0530

    Add basic-calculator-first-exercise code

---
+++ basic-calculator-first-exercise/main.py
@@ -0,0 +1,12 @@
+a = int(input("enter a first no.:"))
+b = int(input("enter a second no. :"))
+c =input("enter the arithmetic operator(+,-,/,*) :")
+if c == "+" :
+  print(a+b)
+elif c == "-":
+  print(a-b)
+elif c == "/":
+  print(a/b)
+elif c == "*" :
+  print(a*b)
+    
