7a33e52b1b099d3c57495b7df802f7d7d382eec8
samyax09 <samyaxdixit8@gmail.com>
2026-08-25 00:27:24 +0530

    Sync current workspace main.py

---
+++ current-workspace/main.py
@@ -0,0 +1,12 @@
+for i in range(3,40,4):
+  print(i)
+  i=i+1
+else:
+  print("end")
+  
+n = int(input("enter a no.: "))
+while n < 40 :
+  n = int(input("enter a no.: "))
+  print(n)
+else:
+  print("end")
